ab042996e74a45f8801662e217337d796f3092ff
b-munar <b.munar@uniandes.edu.co>
2024-07-28 18:20:22 -0500

    cors

--- src/app.py
+++ src/app.py
@@ -2,11 +2,13 @@ from src.controllers.delete_controller import Delete
 from src.controllers.table_controller import GameTable
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from src.controllers.ping_controller import Ping
 from src.controllers.conmebol_controller import DateGameController,DateGameCalendarController
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     api = Api(app)
     api.add_resource(Ping, '/conmebol/ping')
     api.add_resource(DateGameController, '/conmebol')
